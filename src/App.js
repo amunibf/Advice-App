6e989f4d0b7ed3d50ffc0dcd20e2dcc782edea66
amunibf <amunibf@gmail.com>
2020-05-29 12:49:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import './App.css'
+
+
+
+class App extends Component {
+  state = { advice : '' }
+
+  componentDidMount() {
+    console.log('COMPONENT DID MOUNT')
+    this.fetchAdvice()
+  }
+  fetchAdvice = ()=>{
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response)=>{
+          const {advice} = response.data.slip
+          this.setState({advice})
+          console.log(advice)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+  }
+  
+
+
+  render() {
+    const {advice} = this.state
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry : './src/index.js',
+  output : {
+    path : path.resolve(__dirname, 'dist'),
+    filename : 'index.bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  module : {
+      rules :[
+          {
+              test : /\.js$/,
+              exclude : /node_modules/,
+              use : {
+                loader : 'babel-loader'
+              }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use : {
+              loader : 'file-loader'
+            }
+          }
+      ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins : [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    
+  ]
+}
